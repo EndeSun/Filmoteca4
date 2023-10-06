33dc7bd230d775d79671ada84a4cd5f9c8fb06e1
ende <1195562121ende@gmail.com>
2023-10-06 13:36:26 +0200

    Práctica 4 ejercicio 2 terminado

--- app/src/main/java/es/ua/eps/filmoteca/FilmListActivity.kt
+++ app/src/main/java/es/ua/eps/filmoteca/FilmListActivity.kt
@@ -1,9 +1,15 @@
 package es.ua.eps.filmoteca
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import es.ua.eps.filmoteca.databinding.ActivityFilmListBinding
 
 
@@ -87,15 +93,33 @@ class FilmListActivity : AppCompatActivity() {
         }
     }
 
+    class FilmsArrayAdapter (context: Context?, resource:Int, objects: List<Film>): ArrayAdapter<Film>(context!!, resource, objects!!){
+        override fun getView(position: Int, convertView: View?, parent:ViewGroup) : View{
+            var view: View = convertView?: LayoutInflater.from(this.context).inflate(R.layout.item_film, parent, false)
+
+            val tvFilmName = view.findViewById(R.id.filmName) as TextView
+            val tvDirectorName = view.findViewById(R.id.directorName) as TextView
+            val ivFilmIcon = view.findViewById(R.id.filmIcon) as ImageView
+
+            getItem(position) ?.let {
+                tvFilmName.text = it.title
+                tvDirectorName.text = it.director
+                ivFilmIcon.setImageResource(it.imageResId)
+            }
+            return view
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityFilmListBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val listFilm = binding.filmList
 
-        val adapter = ArrayAdapter(
+//        Este adatador predefinido ya no nos sirve, necesitamos crear el nuestro propio
+        val adapter = FilmsArrayAdapter(
             this,
-            android.R.layout.simple_list_item_1,
+            R.layout.item_film,
             FilmDataSource.films
         )
 
